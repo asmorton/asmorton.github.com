23991d9776dcf64367fd36bba371c17c1aea0a69
A Morton <asm.map@gmail.com>
2013-05-05 12:19:13 -0400

    fix

--- final_project/explore/maptest.js
+++ final_project/explore/maptest.js
@@ -1,17 +1,30 @@
 function initialize() {
-  var istanbul = new google.maps.LatLng(41.007884, 28.968887);
-  var mapOptions = {
-    zoom: 8,
-    center: istanbul
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  }
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'PlagueMap.kml'
-  });
-  ctaLayer.setMap(map);
+var istanbul = new google.maps.LatLng(41.007884, 28.968887);
+var mapOptions = {
+  zoom: 5,
+  center: istanbul,
+  mapTypeId: google.maps.MapTypeId.TERRAIN
 }
 
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+var ctaLayer = new google.maps.KmlLayer('http://asmorton.github.io/PlagueMap.kml');
+ctaLayer.setMap(map);
+
+var stylesArray = [
+  {
+    featureType: 'all',
+    elementType: 'labels',
+    stylers: [
+      {visibility: 'off'},
+      // etc...
+    ]
+  },
+  {
+    featureType: '',
+    // etc...
+  }
+]
+}
 google.maps.event.addDomListener(window, 'load', initialize);
+
