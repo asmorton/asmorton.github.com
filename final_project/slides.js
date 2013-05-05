e9cb33bf0f4859bcea1160dad1d127bf9ff781c7
A Morton <asm.map@gmail.com>
2013-05-05 07:32:49 -0400

    added final proj

---
+++ final_project/slides.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+
+$('#slideshow').cycle({ 
+    fx:'turnRight',  
+    fit:0,
+    slideResize:1,
+    timeout:0,
+    next:'#arrow',
+    prevNextEvent:'click.cycle',	   
+
+});
+
+
